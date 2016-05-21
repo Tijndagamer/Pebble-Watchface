1ea3d1e1d86b3789bfa4856f5fd5af216f90d890
Tijndagamer <tijndagamer25@gmail.com>
2016-05-21 18:09:26 +0200

    Splitting the code didn't work. Shit.

--- src/health.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEALTH_H_INCLUDED
-#define HEALTH_H_INCLUDED
-
-#include <pebble.h>
-static void update_step_counter();
-static void health_handler(HealthEventType event, void *context);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,4 @@
 #include <pebble.h>
-#include "health.h"
-#include "main.h"
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
@@ -8,6 +6,44 @@ static TextLayer *s_battery_info_layer;
 static TextLayer *s_step_count_layer;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
+
+static void update_step_counter() {
+    HealthMetric metric = HealthMetricStepCount;
+    time_t start = time_start_of_today();
+    time_t end = time(NULL);
+
+    // Check the metric has data available for today
+    HealthServiceAccessibilityMask mask = health_service_metric_accessible(metric,
+    start, end);
+
+    if(mask & HealthServiceAccessibilityMaskAvailable) {
+        static char steps_count_char[] = "00000000000";
+        snprintf(steps_count_char, sizeof(steps_count_char), "%d", (int)health_service_sum_today(metric));
+        // Data is available!
+        APP_LOG(APP_LOG_LEVEL_INFO, "Steps today: %d", (int)health_service_sum_today(metric));
+        APP_LOG(APP_LOG_LEVEL_INFO, steps_count_char);
+        text_layer_set_text(s_step_count_layer, steps_count_char);
+    } else {
+        // No data recorded yet today
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Data unavailable!");
+    }
+}
+
+static void health_handler(HealthEventType event, void *context) {
+    switch (event) {
+        case HealthEventSignificantUpdate:
+            APP_LOG(APP_LOG_LEVEL_INFO, "Significant update");
+            update_step_counter();
+            break;
+        case HealthEventMovementUpdate:
+            APP_LOG(APP_LOG_LEVEL_INFO, "movement update");
+            update_step_counter();
+            break;
+        case HealthEventSleepUpdate:
+            // This is not important for the step counter, so just ignore it.
+            break;
+    }
+}
 
 static void battery_state_handler(BatteryChargeState charge) {
     // Convert to char
